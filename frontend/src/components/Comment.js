12d5ebb7880407dcc93826570099116d2066ff05
AlexanderBowers <7319abowers@gmail.com>
2021-03-04 14:17:02 -0800

    user can pin comments

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
     const Comment = (props) => {
+
+        function createComment  (comment, jmod)  {
+            let token = localStorage.getItem("token")
+            fetch('http://localhost:3000/comments', {
+                method: "POST",
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                    "Content-Type" : "application/json",
+                    "Accept" : "application/json"
+                },
+                body: JSON.stringify({
+                    body: `${comment.data.body}`,
+                    url: `https://reddit.com/${props.comment.data.permalink}`,
+                    jmod_id: `${jmod.id}`    
+                })
+            })
+            .then(res => res.json())
+            .then(console.log())
+        }
             return (
                 <div className="card" width="15%">
                     <p>{props.comment.data.body}</p>
-                    <a href={`https://reddit.com/${props.comment.data.permalink} `}>{props.comment.data.permalink}</a>
+                    <a href={`https://reddit.com/${props.comment.data.permalink}`}>https://reddit.com/{props.comment.data.permalink}</a>
+                    <button onClick={() => {createComment(props.comment, props.jmod)}} width="15px">pin</button>
                 </div>
 
             )
