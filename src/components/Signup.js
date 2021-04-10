fb5f51f48ff238e182525a6bc202ad57fbdc1c37
AlexanderBowers <7319abowers@gmail.com>
2021-04-10 12:48:58 -0700

    added warning text

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -13,6 +13,9 @@ class Signup extends Component {
     render() {
         return(
             <div className="Login-container">
+                <h5>This website is still in development and is not endorsed by or affiliated with Jagex.<br></br>
+                    <br></br>DO NOT use your RuneScape password for this site.
+                </h5>
                 <Container>
                 <Form>
                     <Form.Label> Signup</Form.Label>
@@ -20,13 +23,16 @@ class Signup extends Component {
                         <Form.Label>Username</Form.Label>
                         <Form.Control name="username" placeholder="Enter username" onChange={(e) => this.props.handleLoginChange(e)}/>
                         <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
+                        Do not use your RuneScape username/email.
                         </Form.Text>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control name="password" type="password" placeholder="Password" onChange={(e) => this.props.handleLoginChange(e)}/>
+                        <Form.Text className="text-muted">
+                        Do not use your RuneScape password.
+                        </Form.Text>
                     </Form.Group>
                     <div className="buttons">
                     <Button variant="success" type="button" className="create" onClick={(e) => this.props.handleSignupSubmit(e)}>
